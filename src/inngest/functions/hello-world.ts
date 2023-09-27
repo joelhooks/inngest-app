e97dc52a106a38f39984c8b6b6b72d99c14f67a7
joel <joelhooks@gmail.com>
2023-09-27 09:30:31 -0700

    hey git

---
+++ src/inngest/functions/hello-world.ts
@@ -0,0 +1,10 @@
+import { inngest } from '../inngest.client'
+
+export const helloWorld = inngest.createFunction(
+    { id: 'hello-world', name: 'Hello World' },
+    { event: 'test/hello.world' },
+    async ({ event, step }) => {
+        await step.sleep('3s')
+        return { event, body: event.data.message }
+    }
+)
